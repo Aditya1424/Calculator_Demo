7a5e2d3906a09bbf734cdc745f723776432f3a65
Aditya Gupta <ag7023587@gmail.com>
2025-01-17 15:57:38 +0530

    smallest code added

--- smallest.java
+++ smallest.java
@@ -1,9 +1,9 @@
-public class largest {
+public class smallest {
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        if(b>a){
-            System.out.println("Largest");
+        if(a<b){
+            System.out.println("Smallest");
         }
     }
 }
--- subtraction.java
+++
@@ -1,7 +0,0 @@
-public class subtraction {
-    public static void main(String[] args) {
-        int a=20;
-        int b=10;
-        System.out.println(a-b);
-    }
-}
