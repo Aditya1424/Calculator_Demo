5a7e23f6cc7c2b22e21b817ff34491d16db293ad
Aditya Gupta <ag7023587@gmail.com>
2025-01-17 15:46:32 +0530

    largest code added

---
+++ largest.java
@@ -0,0 +1,9 @@
+public class largest {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if(b>a){
+            System.out.println("Largest");
+        }
+    }
+}
